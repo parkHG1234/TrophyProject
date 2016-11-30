5e1ef6132dbf1c9bccd2b56dcc8bd483c75ca91f
parkHG1234 <ootu789@naver.com>
2016-11-30 21:15:39 +0900

    Http TRY/Catch 분리

--- app/src/main/java/trophy/projetc2/MainActivity.java
+++ app/src/main/java/trophy/projetc2/MainActivity.java
@@ -1,13 +1,49 @@
 package trophy.projetc2;
 
+import android.os.StrictMode;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import trophy.projetc2.Http.HttpClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[][] pa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar123);
+        toolbar.setTitle("ggg");
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //navigationView.setNavigationItemSelectedListener(this);
+        HttpClient a= new HttpClient();
+        Log.i("결과",a.HttpClient("Web_basket","NaviTeamInfo_Player.jsp","park123"));
+
     }
 }
