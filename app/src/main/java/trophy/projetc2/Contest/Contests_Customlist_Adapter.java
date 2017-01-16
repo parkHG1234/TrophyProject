a75627693c189363bf888bfa71766cdaaec5c672
ldong <hccs611@gmail.com>
2017-01-16 15:01:07 +0900

    대회 리스트 내 남은 일수 표시

--- app/src/main/java/trophy/projetc2/Contest/Contests_Customlist_Adapter.java
+++ app/src/main/java/trophy/projetc2/Contest/Contests_Customlist_Adapter.java
@@ -12,9 +12,14 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import trophy.projetc2.R;
+
+import static java.lang.System.out;
 
 /**
  * Created by KimIkJoong on 2016-11-08.
@@ -51,7 +56,7 @@ public class Contests_Customlist_Adapter extends BaseAdapter {
         if(convertView == null) {
             convertView = inflater.inflate(R.layout.layout_customlist_contests, parent, false);
 
-
+            String Da = " 일";
 
             Contest_Pk = arrData.get(position).getContest_Pk();
             String Contest_Title = arrData.get(position).getContest_Title();
@@ -60,11 +65,16 @@ public class Contests_Customlist_Adapter extends BaseAdapter {
             String Contest_currentNum = arrData.get(position).getContest_currentNum();
             String Contest_maxNum = arrData.get(position).getContest_maxNum();
             String Contest_Payment = arrData.get(position).getContest_Payment();
+            String Contest_RecruitFinishData = arrData.get(position).getRecruitFinishDate();
 
             ImageView viewContest_Image = (ImageView)convertView.findViewById(R.id.Contest_logoImage);
             TextView viewContest_Title = (TextView)convertView.findViewById(R.id.contest_title);
             TextView viewContest_Date = (TextView)convertView.findViewById(R.id.contest_date);
             TextView viewContest_Num = (TextView)convertView.findViewById(R.id.contest_num);
+
+            TextView remainder = (TextView)convertView.findViewById(R.id.contest_list_remainder);
+            TextView remainder1 = (TextView)convertView.findViewById(R.id.contest_list_remainder1);
+            TextView remainder2 = (TextView)convertView.findViewById(R.id.contest_list_remainder2);
             //TextView viewContest_Point = (TextView)convertView.findViewById(R.id.contest_point);
 
             viewContest_Title.setText(Contest_Title);
@@ -74,7 +84,36 @@ public class Contests_Customlist_Adapter extends BaseAdapter {
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .into(viewContest_Image);
-           // viewContest_Point.setText(Contest_Point);
+
+            //남은 일 계산
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yy / MM / dd");
+            try {
+                Date date = dateFormat.parse(Contest_RecruitFinishData);
+                out.println(date);
+                Date currentDay = new Date();
+                out.println("현재시간 : " + currentDay);
+
+                if(currentDay.getTime() < date.getTime()) {
+                    long diff = date.getTime() - currentDay.getTime();
+
+
+                    long diffday = diff / (24 * 60 * 60 * 1000);
+                    if(diff > 7) {
+                        diffday = diffday/7;
+                        Da = " 주";
+                    }
+                    remainder.setText(diffday+Da);
+                }else {
+                    remainder.setText("마감");
+                    remainder1.setVisibility(View.GONE);
+                    remainder2.setVisibility(View.GONE);
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+
+
             convertView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -84,8 +123,6 @@ public class Contests_Customlist_Adapter extends BaseAdapter {
                     context.startActivity(intent);*/
                 }
             });
-
-
         }
 
         return convertView;
